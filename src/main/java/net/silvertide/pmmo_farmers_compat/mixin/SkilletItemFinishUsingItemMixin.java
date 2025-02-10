c6a0cc26da337d5364964f4bb97f7e2ffe2ba9a2
Silvertide7 <natephillips801@gmail.com>
2025-02-10 16:00:59 -0700

    1.0.1 Release - Add SMELT xp for using cooking with skillet in main hand.

--- src/main/java/net/silvertide/pmmo_farmers_compat/mixin/SkilletItemFinishUsingItemMixin.java
+++ src/main/java/net/silvertide/pmmo_farmers_compat/mixin/SkilletItemFinishUsingItemMixin.java
@@ -1,30 +1,34 @@
 package net.silvertide.pmmo_farmers_compat.mixin;
 
-import net.minecraft.core.BlockPos;
+import harmonised.pmmo.api.APIUtils;
+import harmonised.pmmo.api.enums.EventType;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.level.Level;
-import net.silvertide.pmmo_farmers_compat.util.EventUtil;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.fml.LogicalSide;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import vectorwing.farmersdelight.common.block.entity.SkilletBlockEntity;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import vectorwing.farmersdelight.common.item.SkilletItem;
 
-//@Mixin(SkilletItem.class)
-public abstract class SkilletItemFinishUsingItemMixin {
+@Mixin(SkilletItem.class)
+public abstract class SkilletItemFinishUsingItemMixin extends BlockItem {
+    public SkilletItemFinishUsingItemMixin(Block block, Properties properties) {
+        super(block, properties);
+    }
 
-//    @ModifyArg(method = "lambda$finishUsingItem$0(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/crafting/RecipeHolder;)V",
-//            at = @At(value = "INVOKE",
-//                    target = "Lnet/minecraft/world/entity/player/Inventory;add(Lnet/minecraft/world/item/ItemStack;)Z"),
-//            remap = false)
-//    private static ItemStack modifySpawnItemEntityArg(ItemStack resultStack) {
-//        SkilletBlockEntity blockEntity = ((SkilletItem)(Object) this);
-//
-//        if(blockEntity != null) {
-//            Level level = blockEntity.getLevel();
-//            BlockPos pos = blockEntity.getBlockPos();
-//            EventUtil.postFurnaceBurnEvent(level, pos, resultStack);
-//        }
-//        return resultStack;
-//    }
+    @Inject(method = "lambda$finishUsingItem$0(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/crafting/RecipeHolder;)V",
+            at = @At(value = "INVOKE",
+                    target = "net/minecraft/world/item/crafting/CampfireCookingRecipe.assemble (Lnet/minecraft/world/item/crafting/SingleRecipeInput;Lnet/minecraft/core/HolderLookup$Provider;)Lnet/minecraft/world/item/ItemStack;",
+                    shift = At.Shift.AFTER),
+            remap = false)
+    private static void modifySpawnItemEntityArg(ItemStack cookingStack, Level level, Player player, ItemStack stack, RecipeHolder recipe, CallbackInfo ci) {
+        APIUtils.getXpAwardMap(cookingStack, EventType.SMELT, LogicalSide.SERVER, player).forEach((skill, xpChange) -> {
+            APIUtils.addXp(skill, player, xpChange);
+        });
+    }
 }
