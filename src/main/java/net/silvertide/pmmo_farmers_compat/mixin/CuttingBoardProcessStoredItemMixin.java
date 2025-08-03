dc9139b6c30f3a5e2f32235eb55f3f220a5ab731
Silvertide7 <natephillips801@gmail.com>
2025-08-03 17:00:45 -0600

    1.0.2 Release - Removed refmap

--- src/main/java/net/silvertide/pmmo_farmers_compat/mixin/CuttingBoardProcessStoredItemMixin.java
+++ src/main/java/net/silvertide/pmmo_farmers_compat/mixin/CuttingBoardProcessStoredItemMixin.java
@@ -1,29 +1,45 @@
 package net.silvertide.pmmo_farmers_compat.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.silvertide.pmmo_farmers_compat.util.EventUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import vectorwing.farmersdelight.common.block.entity.CuttingBoardBlockEntity;
 
-@Mixin(CuttingBoardBlockEntity.class)
+@Mixin(
+        value = {CuttingBoardBlockEntity.class},
+        remap = false
+)
 public abstract class CuttingBoardProcessStoredItemMixin {
-    @ModifyArg(method = "lambda$processStoredItemUsingTool$2(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/crafting/RecipeHolder;)V",
-            at = @At(value = "INVOKE",
-                    target = "vectorwing/farmersdelight/common/utility/ItemUtils.spawnItemEntity (Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;DDDDDD)V"),
-            remap = false)
-    public ItemStack modifySpawnItemEntityArg(ItemStack resultStack) {
+    public CuttingBoardProcessStoredItemMixin() {}
+
+    @ModifyArg(
+            method = {"lambda$processStoredItemUsingTool$2"},
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lvectorwing/farmersdelight/common/utility/ItemUtils;spawnItemEntity(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;DDDDDD)V"
+            ),
+            index = 1,
+            order = 1100
+    )
+    public ItemStack modifySpawnItemEntityArg(ItemStack stack, @Local(argsOnly = true) Player player) {
         CuttingBoardBlockEntity cuttingBoardBlockEntity = ((CuttingBoardBlockEntity)(Object)this);
         if(cuttingBoardBlockEntity != null) {
             Level level = cuttingBoardBlockEntity.getLevel();
-            BlockPos pos = cuttingBoardBlockEntity.getBlockPos();
-            for(int i = 0; i < resultStack.getCount(); i++) {
-                EventUtil.postPlayerCraftEvent(level, pos, resultStack);
+            for(int i = 0; i < stack.getCount(); i++) {
+                if(level != null) {
+                    NeoForge.EVENT_BUS.post(new PlayerEvent.ItemCraftedEvent(player, stack, new SimpleContainer(0)));
+                }
             }
         }
-        return resultStack;
+        return stack;
     }
 }
