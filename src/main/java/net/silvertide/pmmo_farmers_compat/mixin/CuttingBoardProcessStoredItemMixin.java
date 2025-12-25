ecb71ed86b28b78756ad2f370788674218dd5bea
Silvertide7 <natephillips801@gmail.com>
2025-12-24 23:43:05 -0700

    remove space

--- src/main/java/net/silvertide/pmmo_farmers_compat/mixin/CuttingBoardProcessStoredItemMixin.java
+++ src/main/java/net/silvertide/pmmo_farmers_compat/mixin/CuttingBoardProcessStoredItemMixin.java
@@ -11,7 +11,7 @@ import vectorwing.farmersdelight.common.block.entity.CuttingBoardBlockEntity;
 
 @Mixin(CuttingBoardBlockEntity.class)
 public abstract class CuttingBoardProcessStoredItemMixin {
-    @ModifyArg(method = "lambda$processStoredItemUsingTool$2(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/player/Player;Lvectorwing/farmersdelight/common/crafting/CuttingBoardRecipe;)V", at = @At(value = "INVOKE", target = "vectorwing/farmersdelight/common/utility/ItemUtils.spawnItemEntity (Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;DDDDDD)V"), remap = false)
+    @ModifyArg(method = "lambda$processStoredItemUsingTool$2(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/player/Player;Lvectorwing/farmersdelight/common/crafting/CuttingBoardRecipe;)V", at = @At(value = "INVOKE", target = "vectorwing/farmersdelight/common/utility/ItemUtils.spawnItemEntity(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;DDDDDD)V"), remap = false)
     public ItemStack modifySpawnItemEntityArg(ItemStack resultStack){
         Level level = ((CuttingBoardBlockEntity)(Object)this).getLevel();
         BlockPos pos = ((CuttingBoardBlockEntity)(Object)this).getBlockPos();
